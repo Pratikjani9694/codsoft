c7cc6d034f1b1d63e95206ed88a838d5295a0daf
Pratikjani9694 <janidada9694@gmail.com>
2024-07-28 11:55:55 +0530

    ATM.java

---
+++ task 3/ATM.java
@@ -0,0 +1,101 @@
+import java.util.Scanner;
+
+public class ATM {
+    private BankAccount account;
+    private Scanner scanner;
+
+    public ATM(BankAccount account) {
+        this.account = account;
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void start() {
+        boolean quit = false;
+        while (!quit) {
+            System.out.println("\nWelcome to the ATM");
+            System.out.println("1. Check Balance");
+            System.out.println("2. Deposit");
+            
+            System.out.println("3. Withdraw");
+            System.out.println("4. Quit");
+            System.out.print("Please choose an option: ");
+
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    checkBalance();
+                    break;
+                case 2:
+                    deposit();
+                    break;
+                case 3:
+                    withdraw();
+                    break;
+                case 4:
+                    quit = true;
+                    System.out.println("Thank you for using the ATM. Goodbye!");
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+        }
+    }
+
+    private void checkBalance() {
+        System.out.println("Your current balance is: $" + account.getBalance());
+    }
+
+    private void deposit() {
+        System.out.print("Enter the amount to deposit: ");
+        double amount = scanner.nextDouble();
+        account.deposit(amount);
+    }
+
+    private void withdraw() {
+        System.out.print("Enter the amount to withdraw: ");
+        double amount = scanner.nextDouble();
+        account.withdraw(amount);
+    }
+
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount(500.00); // Initial balance of $500
+        ATM atm = new ATM(account);
+        atm.start();
+    }
+}
+class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposit successful. New balance: $" + balance);
+        } else {
+            System.out.println("Invalid deposit amount. Please try again.");
+        }
+    }
+
+    public boolean withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawal successful. New balance: $" + balance);
+            return true;
+        } else if (amount > balance) {
+            System.out.println("Insufficient funds. Withdrawal failed.");
+            return false;
+        } else {
+            System.out.println("Invalid withdrawal amount. Please try again.");
+            return false;
+        }
+    }
+}
+
+
